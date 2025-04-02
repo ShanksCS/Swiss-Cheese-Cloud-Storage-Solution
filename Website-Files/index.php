bf2f210f766df3708db36f2f91e75bd46d1f281b
ShanksCS <116257069+ShanksCS@users.noreply.github.com>
2025-04-02 22:33:36 +0100

    Converted login page to .php
    Added error message if username and password is wrong

---
+++ Website-Files/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>SCSS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="main">
+        <!-- In your HTML -->
+        <div class="logo-wrapper">
+          <img src="img/logo.png" alt="Swiss Cheese Storage Solution" />
+        </div>
+
+        <h3>Enter your login credentials</h3>
+
+        <form action="login.php" method="post">
+			<?php if (isset($_GET['error'])): ?>
+				<div class="error" style="color: red; margin-bottom: 10px;">
+					<?php echo htmlspecialchars($_GET['error']); ?>
+			</div>
+		<?php endif; ?>
+
+		<label for="first">Username:</label>
+			<input type="text" id="first" name="first"
+			placeholder="Enter your Username" required>
+
+		<label for="password">Password:</label>
+			<input type="password" id="password" name="password"
+			placeholder="Enter your Password" required>
+        
+		<div class="wrap">
+			<button type="submit">Submit</button>
+        </div>
+        </form>
+    </div>
+</body>
+
+</html>
