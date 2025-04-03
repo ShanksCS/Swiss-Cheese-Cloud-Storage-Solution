31a50892e66d9edc0c98c588077fe2673c57b85d
ShanksCS <116257069+ShanksCS@users.noreply.github.com>
2025-04-03 17:21:49 +0100

    Changed direct to login

--- Website-Files/index.php
+++ Website-Files/index.php
@@ -1,11 +1,15 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 session_start();
+
+// Check if the user is logged in
 if (isset($_SESSION['username'])) {
     header("Location: upload.php");
+    exit();
 } else {
-    header("Location: login.php");
+    header("Location: login.html");
+    exit();
 }
-exit();
 ?>
