a9269f072a7707299f1abbe2ab7741315546f1cd
ShanksCS <116257069+ShanksCS@users.noreply.github.com>
2025-04-05 18:57:33 +0100

    Update logout.php

--- Website-Files/logout.php
+++ Website-Files/logout.php
@@ -1,7 +1,27 @@
 <?php
 session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
+// Destroy the session
 session_unset();
 session_destroy();
+
+// Clear session cookie explicitly
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(), '', time() - 42000,
+        $params['path'], $params['domain'],
+        $params['secure'], $params['httponly']
+    );
+}
+
+// Extra fallback
+setcookie("PHPSESSID", "", time() - 3600, "/");
+
+// Redirect to login
 header("Location: login.php");
 exit();
 ?>
