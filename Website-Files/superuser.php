a19e5e5303e8c29462b73eb4129ea4dd39fe9f13
ShanksCS <116257069+ShanksCS@users.noreply.github.com>
2025-04-05 19:06:59 +0100

    Update superuser.php

--- Website-Files/superuser.php
+++ Website-Files/superuser.php
@@ -55,6 +55,11 @@ $users = $conn->query("SELECT username, name, password, created_at, last_login F
         <h1>Welcome, <?= htmlspecialchars($_SESSION['username']) ?> (Superuser)</h1>
         <div class="file-list">
             <h3>User Management Table</h3>
+			<div style="text-align: right; margin: 10px;">
+				<form action="logout.php" method="POST" style="margin-top: 20px;">
+					<button type="submit" style="background-color: red; color: white;">Logout</button>
+				</form>
+			</div>
             <table border="1" cellpadding="10" cellspacing="0" style="width: 100%;">
                 <tr>
                     <th>Username</th>
@@ -91,11 +96,6 @@ $users = $conn->query("SELECT username, name, password, created_at, last_login F
         </div>
     </div>
 </form>
-	<div style="text-align: right; margin: 10px;">
-        <form action="logout.php" method="POST" style="margin-top: 20px;">
-            <button type="submit" style="background-color: red; color: white;">Logout</button>
-        </form>
-	</div>
 </body>
 </html>
 
