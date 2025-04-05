ee9df5683ae3d3dcd9eaae60d8b61717c1e52101
ShanksCS <116257069+ShanksCS@users.noreply.github.com>
2025-04-05 18:14:55 +0100

    Using JS to fix logout button

--- Website-Files/superuser.php
+++ Website-Files/superuser.php
@@ -87,11 +87,9 @@ $users = $conn->query("SELECT username, name, password, created_at, last_login F
     
 
     <!-- Logout button placed outside all forms -->
-    <form action="logout.php" method="POST" style="position: fixed; bottom: 30px; right: 30px;">
-        <button type="submit" style="background-color: red; color: white; padding: 10px 20px; border: none; border-radius: 8px;">
-            Logout
-        </button>
-    </form>
+	<button onclick="window.location.href='logout.php'" style="position: fixed; bottom: 30px; right: 30px; background-color: red; color: white; padding: 10px 20px; border: none; border-radius: 8px;">
+		Logout
+	</button>
 </body>
 </html>
 
