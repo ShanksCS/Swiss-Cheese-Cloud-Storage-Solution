0c85333d515ae1013df280daae50bb9578521ff4
ShanksCS <116257069+ShanksCS@users.noreply.github.com>
2025-04-05 19:14:02 +0100

    Update superuser.php

--- Website-Files/superuser.php
+++ Website-Files/superuser.php
@@ -57,7 +57,9 @@ $users = $conn->query("SELECT username, name, password, created_at, last_login F
             <h3>User Management Table</h3>
 			<div style="text-align: left; margin: 10px;">
 				<form action="logout.php" method="POST" style="margin-top: 20px;">
-					<button type="submit" style="background-color: red; color: white;">Logout</button>
+					<button type="submit" style="background-color: green; color: white; font-size: 12px; padding: 4px 8px; border: none; border-radius: 4px;">
+						Logout
+					</button>
 				</form>
 			</div>
             <table border="1" cellpadding="10" cellspacing="0" style="width: 100%;">
